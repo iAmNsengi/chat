58602382bf6720cb948223f77dbdd64fad143db6
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-19 17:12:21 +0200

    feat: addfriend button zod validation

--- client/src/components/AddFriendButton.tsx
+++ client/src/components/AddFriendButton.tsx
@@ -1,18 +1,54 @@
 "use client"
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import Button from "./ui/Button";
+import { addFriendValidator } from "@/lib/validations/add-friend";
+import axios, { AxiosError } from "axios";
+import { z } from "zod";
+import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 
 interface AddFriendButtonProps {
     
 }
 
-const AddFriendButton:FC<AddFriendButtonProps> = ({}) => {
+type FormData = z.infer<typeof addFriendValidator>
+
+const AddFriendButton: FC<AddFriendButtonProps> = ({ }) => {
+    
+    const [showSuccessMsg, setShowSuccessMsg] = useState<boolean>(false)
+    const {register,  handleSubmit, setError} = useForm<FormData>({resolver:zodResolver(addFriendValidator)})
+
+
+    const addFriend = async (email: string) => {
+        try {
+            const validatedEmail = addFriendValidator.parse({ email })
+            
+            await axios.post('/api/friends', {
+                email:validatedEmail
+            })
+
+            setShowSuccessMsg(true)
+        } catch (error) {
+            console.error(error)            
+            if (error instanceof z.ZodError) {
+                setError('email', {message:error.message})
+                return
+            }
+            if (error instanceof AxiosError) {
+                setError('email', { message: error.response?.data })
+                return
+            }
+            
+        }
+    }
     
     return <form className="max-w-sm px-4">
         <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Add friend by E-mail</label>
         <div className="mt-2 flex gap-4">
-            <input type="text" className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 " placeholder="email@example.com"/>
+            <input {...register('email')} type="text" className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 " placeholder="email@example.com"/>
             
         <Button className="px-5">Add</Button>
         </div>
