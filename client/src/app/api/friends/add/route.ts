547f486534f6e4a04e0dc99faadad015c77150a5
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-22 19:22:31 +0200

    fix: add friend UI & addfriend route

--- client/src/app/api/friends/add/route.ts
+++ client/src/app/api/friends/add/route.ts
@@ -1,6 +1,9 @@
+import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
 import { addFriendValidator } from "@/lib/validations/add-friend"
 import { getServerSession } from "next-auth";
+import { z } from "zod";
 
 export async function POST(req: Request){
     try {
@@ -26,9 +29,24 @@ export async function POST(req: Request){
         if (!idToAdd) return new Response("User with given email doesn't exist", { status: 400 })
         if(idToAdd === session.user.id) return new Response("You can't add yourself as a friend", {status:400})
 
-        return new Response('Request sent', {status: 200})
+        // check if user is already added
+        const isAlreadyAdded = await fetchRedis('sismember', `user:${idToAdd}:incoming_friend_requests`, session.user.id) as 0 | 1
+        
+        if (isAlreadyAdded) return new Response("Already added user", {status: 400})
+
+        // already friends
+        const isAlreadyFriends = await fetchRedis('sismember', `user:${idToAdd}:friends`, session.user.id) as 0 | 1
+        if (isAlreadyFriends) return new Response("Can't add your friend again!", { status: 400 })
+        
+        // valid request
+        db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id)
+        return new Response("Adding friends done successfully!")
+
         
     } catch (error) {
-        console.log(error)
+        if (error instanceof z.ZodError) {
+            return new Response("Invalid request", {status:422})
+        }
+        return new Response("Invalid request", {status:400})
     }
 }
--- client/src/components/AddFriendButton.tsx
+++ client/src/components/AddFriendButton.tsx
@@ -20,6 +20,7 @@ type FormData = z.infer<typeof addFriendValidator>
 const AddFriendButton: FC<AddFriendButtonProps> = ({ }) => {
     
     const [showSuccessMsg, setShowSuccessMsg] = useState<boolean>(false)
+    const [showLoading, setShowLoading] = useState<boolean>(false)
     const {register,  handleSubmit, setError, formState:{errors}} = useForm<FormData>({resolver:zodResolver(addFriendValidator)})
 
 
@@ -41,22 +42,20 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({ }) => {
                 return
             }
             setError('email', {message: "Something Went Wrong"})
-            
+        } finally {
+            setShowLoading(false)
         }
     }
 
     const onSubmit = (data: FormData) => {
+        setShowLoading(true)
+        setShowSuccessMsg(false)
         addFriend(data.email)
     }
     
     return <form className="max-w-sm px-4" onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Add friend by E-mail</label>
-        <div className="mt-2 flex gap-4">
-            <input {...register('email')} type="text" className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 " placeholder="email@example.com"/>
-            
-        <Button className="px-5">Add</Button>
-        </div>
-            {errors.email && 
+           {errors.email && 
         <p className="mt-2 text-xs border border-red-500 rounded-md px-3  bg-red-100 font-bold  text-red-600">
         {errors.email.message}
         </p>
@@ -66,6 +65,12 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({ }) => {
        Friend request sent
         </p>
         }
+        <div className="mt-2 flex gap-4">
+            <input required {...register('email')} type="text" className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 " placeholder="email@example.com"/>
+            
+        <Button type="submit" className="px-5" isLoading={showLoading}>Add</Button>
+        </div>
+         
     </form>
 };
 
